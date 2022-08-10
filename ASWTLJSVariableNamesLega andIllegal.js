4e82431659ebf2cb7d4812b54eb6b122ff2cc215
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-10 00:51:11 -0400

    inital commit

---
+++ ASWTLJSVariableNamesLega andIllegal.js
@@ -0,0 +1,12 @@
+// Type the illegal character in this statement.
+// var product cost = 3.45;
+space
+// Type the illegal character in this statement.
+// var P2_a%88$;
+
+// Rewrite this using camelCase.
+// var Nameofband;
+var nameOfBand;
+// Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers. Don't repeat any of them.   
+// var $_;
+
