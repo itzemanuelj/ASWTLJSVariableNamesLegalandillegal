fea10c5111d0a68552851a72c902e538dc11a580
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-12 12:35:12 -0400

    final commit

--- ASWTLJSVariableNamesLega andIllegal.js
+++ ASWTLJSVariableNamesLega andIllegal.js
@@ -1,12 +1,64 @@
 // Type the illegal character in this statement.
 // var product cost = 3.45;
 space
+
 // Type the illegal character in this statement.
 // var P2_a%88$;
+//%
 
 // Rewrite this using camelCase.
 // var Nameofband;
 var nameOfBand;
-// Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers. Don't repeat any of them.   
-// var $_;
 
+// Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers. Don't repeat any of them.   
+    // var $_; or var _$;
+
+// In a single statement, declare the variable firstLegalChar and assign it a string. The string consists entirely of the first character on the keyboard in the top row that's legal in a variable name, as long as the character doesn't come first.
+var firstLegalChar = "1";
+
+// Declare a variable, in camelCase, that combines the two syllables boo and hoo.
+var booHoo;
+
+// In a single statement declare a legally-named variable and assign a number to it.
+// var const = 100;
+
+// In a single statement declare a legally-named variable and assign to it the sum of 2 other legally-named variables.
+var total = profit + time;
+
+// Declare a variable whose name is the combination of 3 characters, in this order: a legal first character that is not a letter, a character that would be illegal as a first character, and a character that would be legal anywhere in the name.
+var _a2;
+
+// The variable boogieWoogie has already been declared. Assign to it the value of another variable, woogieBoogie.
+boogieWoogie = woogieBoogie;
+
+// Declare a variable that is a combination of your first and last names. Use camelCase.
+var justinEmanuelJones;
+
+// Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers
+var _$
+
+//In a single statement declare a legally-named variable and assign a number to it.
+var score = "100";
+
+// In a single statement declare a legally-named variable and assign to it the sum of 2 other legally-named variables.
+var prfoit = total + cost;
+
+// Declare a variable whose name is the combination of 3 characters, in this order: a legal first character that is not a letter, a character that would be illegal as a first character, and a character that would be legal anywhere in the name.
+var _$2;
+
+// The variable boogieWoogie has already been declared. Assign to it the value of another variable, woogieBoogie.
+boogieWoogie = "woogieBooie";
+
+// Declare a variable that is a combination of your first and last names. Use camelCase. If you have a complex name, simplify it. Use "Sarah Billings" instead of "Sarah Sue Billings-Coopersmith III"
+var justinEmanuel;
+
+// Declare a variable that is a combination of your first and last names. Use camelCase. If you have a complex name, simplify it. Use "Sarah Billings" instead of "Sarah Sue Billings-Coopersmith III"
+var justinEmanuelJones = "Justin Jones";
+alert(justinEmanuelJones);
+
+// Declare a variable that combines a number, an alphabet letter, and one of the legal characters that are neither an alphabet letter nor a number.
+// Assign to the variable a string consisting of another legal character that is neither an alphabet letter nor a number.
+// Display the variable's value in an alert, specifying the variable, not the value, as the message.
+var a_1;
+a_1 = "$";
+alert(a_1);
